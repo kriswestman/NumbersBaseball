d5ff0791de4231d4a6f69da9321974156997534d
efross <efross7@gmail.com>
2017-05-15 15:57:58 -0700

    Optimized classes and fixed errors present.

--- src/BaseballGame.java
+++ src/BaseballGame.java
@@ -4,7 +4,6 @@ import java.awt.*;
 public class BaseballGame {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        int strike = 0;
         int swing = 0;
         System.out.print("Welcome to Probability Baseball!\nTo play, enter numbers into the console.\nCan you beat the computer?\n\n");
         Batting bat = new Batting();
@@ -18,7 +17,7 @@ public class BaseballGame {
         while (!bat.strike().equals("Out!") || !bat.normalPitch().equals("Single!") || !bat.normalPitch().equals("Double!")
             || !bat.normalPitch().equals("Triple!") || !bat.normalPitch().equals("Home Run!") || !bat.normalPitch().equals("Walk!")) {
             bat.normalPitch();
-            //test
+
         }
     }
 }
